a4444f83e00aea3ed0d272d8e3443165530d0eee
munishgaurav5 <munishgaurav5@gmail.com>
2017-02-15 23:24:50 +0530

    Update transmission.sh

--- transmission.sh
+++ transmission.sh
@@ -15,25 +15,35 @@ mkdir -p /home/$uname/Downloads/
 chown -R $uname.$uname /home/$uname/Downloads/
 chmod g+w /home/$uname/Downloads/
 
-# Install the firewall (CSF)
-cd /usr/local/src
-wget http://configserver.com/free/csf.tgz
-tar xzf csf.tgz
-cd csf
-./install.generic.sh
-cd /etc/csf
-sed -i 's/^TESTING =.*/TESTING = "0"/' csf.conf
-sed -i 's/^TCP_IN =.*/TCP_IN = "22,80,9091,10000,51413"/' csf.conf
-sed -i 's/^TCP_OUT =.*/TCP_OUT = "1:65535"/' csf.conf
-sed -i 's/^UDP_IN =.*/UDP_IN = "51413"/' csf.conf
-sed -i 's/^UDP_OUT =.*/UDP_OUT = "1:65535"/' csf.conf
-csf -r
+## Install the firewall (CSF)
+#cd /usr/local/src
+#wget http://configserver.com/free/csf.tgz
+#tar xzf csf.tgz
+#cd csf
+#./install.generic.sh
+#cd /etc/csf
+#sed -i 's/^TESTING =.*/TESTING = "0"/' csf.conf
+#sed -i 's/^TCP_IN =.*/TCP_IN = "22,80,9091,10000,51413"/' csf.conf
+#sed -i 's/^TCP_OUT =.*/TCP_OUT = "1:65535"/' csf.conf
+#sed -i 's/^UDP_IN =.*/UDP_IN = "51413"/' csf.conf
+#sed -i 's/^UDP_OUT =.*/UDP_OUT = "1:65535"/' csf.conf
+#csf -r
+
+# Install libevent
+#cd /usr/local/src
+#wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
+#tar xzf libevent-2.0.22-stable.tar.gz
+#cd libevent-2.0.22-stable
+#./configure --prefix=/usr
+#make
+#make install
+
 
 # Install libevent
 cd /usr/local/src
-wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
-tar xzf libevent-2.0.22-stable.tar.gz
-cd libevent-2.0.22-stable
+wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
+tar xzf libevent-2.1.8-stable.tar.gz
+cd libevent-2.1.8-stable
 ./configure --prefix=/usr
 make
 make install
@@ -45,13 +55,23 @@ ldconfig
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 
 # Install transmission
+#cd /usr/local/src
+#wget https://transmission.cachefly.net/transmission-2.84.tar.xz
+#tar xvf transmission-2.84.tar.xz
+#cd transmission-2.84
+#./configure --prefix=/usr
+#make
+#make install
+
+# Install transmission
 cd /usr/local/src
-wget https://transmission.cachefly.net/transmission-2.84.tar.xz
-tar xvf transmission-2.84.tar.xz
-cd transmission-2.84
+wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.92.tar.xz
+tar xvf transmission-2.92.tar.xz
+cd transmission-2.92
 ./configure --prefix=/usr
 make
 make install
+
 
 # Set up init script for transmission-daemon
 cd /etc/init.d
